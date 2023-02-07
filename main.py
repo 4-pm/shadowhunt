0d22085e66aef6d21d4e3be61c58aa55d1b071a9
verdenkaa <nikitapahomov97@gmail.com>
2023-02-07 20:05:12 +0300

    working detecting

--- main.py
+++ main.py
@@ -1,117 +1,221 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import os
+import time
+import glob
+import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_addons as tfa
 
-image_size = (180, 180)
-batch_size = 128
+from skimage import measure
+from skimage.io import imread, imsave, imshow
+from skimage.transform import resize
+from skimage.filters import gaussian
+from skimage.morphology import dilation, disk
+from skimage.draw import polygon, polygon_perimeter
 
-train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
-    "./data/training_data/",
-    validation_split=0.2,
-    subset="both",
-    seed=1337,
-    image_size=image_size,
-    batch_size=batch_size,
-)
+#print(f'Tensorflow version {tf.__version__}')
+#print(f'GPU is {"ON" if tf.config.list_physical_devices("GPU") else "OFF" }')
 
-data_augmentation = keras.Sequential(
-    [
-        layers.RandomFlip("horizontal"),
-        layers.RandomRotation(0.1),
-    ]
-)
-
-'''plt.figure(figsize=(10, 10))
-for images, _ in train_ds.take(1):
-    for i in range(9):
-        augmented_images = data_augmentation(images)
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(augmented_images[0].numpy().astype("uint8"))
-        plt.axis("off")
-
-plt.show()'''
-
-augmented_train_ds = train_ds.map(
-    lambda x, y: (data_augmentation(x, training=True), y))
-
-train_ds = train_ds.map(
-    lambda img, label: (data_augmentation(img), label),
-    num_parallel_calls=tf.data.AUTOTUNE,
-)
-# Prefetching samples in GPU memory helps maximize GPU utilization.
-train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
-val_ds = val_ds.prefetch(tf.data.AUTOTUNE)
+CLASSES = 1
+COLORS = ['red']
+SAMPLE_SIZE = (256, 256)
+OUTPUT_SIZE = (1080, 1920)
 
 
-def make_model(input_shape, num_classes):
-    inputs = keras.Input(shape=input_shape)
+def load_images(image, mask):
+    image = tf.io.read_file(image)
+    image = tf.io.decode_jpeg(image)
+    image = tf.image.resize(image, OUTPUT_SIZE)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = image / 255.0
 
-    # Entry block
-    x = layers.Rescaling(1.0 / 255)(inputs)
-    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    mask = tf.io.read_file(mask)
+    mask = tf.io.decode_png(mask)
+    mask = tf.image.rgb_to_grayscale(mask)
+    mask = tf.image.resize(mask, OUTPUT_SIZE)
+    mask = tf.image.convert_image_dtype(mask, tf.float32)
 
-    previous_block_activation = x  # Set aside residual
+    masks = []
 
-    for size in [256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+    for i in range(CLASSES):
+        masks.append(tf.where(tf.equal(mask, float(i)), 1.0, 0.0))
 
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+    masks = tf.stack(masks, axis=2)
+    masks = tf.reshape(masks, OUTPUT_SIZE + (CLASSES,))
 
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.GlobalAveragePooling2D()(x)
-    if num_classes == 2:
-        activation = "sigmoid"
-        units = 1
-    else:
-        activation = "softmax"
-        units = num_classes
-
-    x = layers.Dropout(0.5)(x)
-    outputs = layers.Dense(units, activation=activation)(x)
-    return keras.Model(inputs, outputs)
+    return image, masks
 
 
-model = make_model(input_shape=image_size + (3,), num_classes=2)
-keras.utils.plot_model(model)
+def augmentate_images(image, masks):
+    random_crop = tf.random.uniform((), 0.3, 1)
+    image = tf.image.central_crop(image, random_crop)
+    masks = tf.image.central_crop(masks, random_crop)
+
+    random_flip = tf.random.uniform((), 0, 1)
+    if random_flip >= 0.5:
+        image = tf.image.flip_left_right(image)
+        masks = tf.image.flip_left_right(masks)
+
+    image = tf.image.resize(image, SAMPLE_SIZE)
+    masks = tf.image.resize(masks, SAMPLE_SIZE)
+
+    return image, masks
 
 
 
-epochs = 15
+images = sorted(glob.glob('./data/peoples/*.jpg'))
+masks = sorted(glob.glob('./data/masks/*.jpg'))
 
-callbacks = [
-    keras.callbacks.ModelCheckpoint("save_at_{epoch}.keras"),
+images_dataset = tf.data.Dataset.from_tensor_slices(images)
+masks_dataset = tf.data.Dataset.from_tensor_slices(masks)
+
+dataset = tf.data.Dataset.zip((images_dataset, masks_dataset))
+
+dataset = dataset.map(load_images, num_parallel_calls=tf.data.AUTOTUNE)
+dataset = dataset.repeat(60)
+dataset = dataset.map(augmentate_images, num_parallel_calls=tf.data.AUTOTUNE)
+
+images_and_masks = list(dataset.take(5))
+
+fig, ax = plt.subplots(nrows=2, ncols=5, figsize=(15, 5), dpi=125)
+
+'''for i, (image, masks) in enumerate(images_and_masks):
+    ax[0, i].set_title('Image')
+    ax[0, i].set_axis_off()
+    ax[0, i].imshow(image)
+
+    ax[1, i].set_title('Mask')
+    ax[1, i].set_axis_off()
+    ax[1, i].imshow(image / 1.5)
+
+    for channel in range(CLASSES):
+        contours = measure.find_contours(np.array(masks[:, :, channel]))
+        for contour in contours:
+            ax[1, i].plot(contour[:, 1], contour[:, 0], linewidth=1, color=COLORS[channel])
+
+plt.show()
+plt.close()'''
+
+train_dataset = dataset.take(2000).cache()
+test_dataset = dataset.skip(2000).take(100).cache()
+
+train_dataset = train_dataset.batch(16)
+test_dataset = test_dataset.batch(16)
+
+
+def input_layer():
+    return tf.keras.layers.Input(shape=SAMPLE_SIZE + (3,))
+
+
+def downsample_block(filters, size, batch_norm=True):
+    initializer = tf.keras.initializers.GlorotNormal()
+
+    result = tf.keras.Sequential()
+
+    result.add(
+        tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                               kernel_initializer=initializer, use_bias=False))
+
+    if batch_norm:
+        result.add(tf.keras.layers.BatchNormalization())
+
+    result.add(tf.keras.layers.LeakyReLU())
+    return result
+
+
+def upsample_block(filters, size, dropout=False):
+    initializer = tf.keras.initializers.GlorotNormal()
+
+    result = tf.keras.Sequential()
+
+    result.add(
+        tf.keras.layers.Conv2DTranspose(filters, size, strides=2, padding='same',
+                                        kernel_initializer=initializer, use_bias=False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if dropout:
+        result.add(tf.keras.layers.Dropout(0.25))
+
+    result.add(tf.keras.layers.ReLU())
+    return result
+
+
+def output_layer(size):
+    initializer = tf.keras.initializers.GlorotNormal()
+    return tf.keras.layers.Conv2DTranspose(CLASSES, size, strides=2, padding='same',
+                                           kernel_initializer=initializer, activation='sigmoid')
+
+
+inp_layer = input_layer()
+
+downsample_stack = [
+    downsample_block(64, 4, batch_norm=False),
+    downsample_block(128, 4),
+    downsample_block(256, 4),
+    downsample_block(512, 4),
+    downsample_block(512, 4),
+    downsample_block(512, 4),
+    downsample_block(512, 4),
 ]
-model.compile(
-    optimizer=keras.optimizers.Adam(1e-3),
-    loss="binary_crossentropy",
-    metrics=["accuracy"],
-)
-model.fit(
-    train_ds,
-    epochs=epochs,
-    callbacks=callbacks,
-    validation_data=val_ds,
-)
 
-model.save("./model/model.h5")
+upsample_stack = [
+    upsample_block(512, 4, dropout=True),
+    upsample_block(512, 4, dropout=True),
+    upsample_block(512, 4, dropout=True),
+    upsample_block(256, 4),
+    upsample_block(128, 4),
+    upsample_block(64, 4)
+]
+
+out_layer = output_layer(4)
+
+# Реализуем skip connections
+x = inp_layer
+
+downsample_skips = []
+
+for block in downsample_stack:
+    x = block(x)
+    downsample_skips.append(x)
+
+downsample_skips = reversed(downsample_skips[:-1])
+
+for up_block, down_block in zip(upsample_stack, downsample_skips):
+    x = up_block(x)
+    x = tf.keras.layers.Concatenate()([x, down_block])
+
+out_layer = out_layer(x)
+
+unet_like = tf.keras.Model(inputs=inp_layer, outputs=out_layer)
+
+#tf.keras.utils.plot_model(unet_like, show_shapes=True, dpi=72)
+
+def dice_mc_metric(a, b):
+    a = tf.unstack(a, axis=3)
+    b = tf.unstack(b, axis=3)
+
+    dice_summ = 0
+
+    for i, (aa, bb) in enumerate(zip(a, b)):
+        numenator = 2 * tf.math.reduce_sum(aa * bb) + 1
+        denomerator = tf.math.reduce_sum(aa + bb) + 1
+        dice_summ += numenator / denomerator
+
+    avg_dice = dice_summ / CLASSES
+
+    return avg_dice
 
 
+def dice_mc_loss(a, b):
+    return 1 - dice_mc_metric(a, b)
+
+
+def dice_bce_mc_loss(a, b):
+    return 0.3 * dice_mc_loss(a, b) + tf.keras.losses.binary_crossentropy(a, b)
+
+
+unet_like.compile(optimizer='adam', loss=[dice_bce_mc_loss], metrics=[dice_mc_metric])
+history_dice = unet_like.fit(train_dataset, validation_data=test_dataset, epochs=25, initial_epoch=0)
+
+unet_like.save_weights('./model/')
--- test.py
+++ test.py
@@ -1,26 +1,131 @@
+from skimage import measure
+from skimage.io import imread, imsave, imshow
+from skimage.transform import resize
+from skimage.filters import gaussian
+from skimage.morphology import dilation, disk
+from skimage.draw import polygon, polygon_perimeter
+from os import walk
+import glob
 import tensorflow as tf
-from tensorflow import keras
-from keras.preprocessing import image
-import numpy
+import numpy as np
+from matplotlib import pyplot as plt
 
-image_size = (180, 180)
-batch_size = 128
+CLASSES = 1
+COLORS = ['red']
+SAMPLE_SIZE = (256, 256)
+#OUTPUT_SIZE = (1080, 1920)
+
+def input_layer():
+    return tf.keras.layers.Input(shape=SAMPLE_SIZE + (3,))
+
+
+def downsample_block(filters, size, batch_norm=True):
+    initializer = tf.keras.initializers.GlorotNormal()
+
+    result = tf.keras.Sequential()
+
+    result.add(
+        tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                               kernel_initializer=initializer, use_bias=False))
+
+    if batch_norm:
+        result.add(tf.keras.layers.BatchNormalization())
+
+    result.add(tf.keras.layers.LeakyReLU())
+    return result
+
+
+def upsample_block(filters, size, dropout=False):
+    initializer = tf.keras.initializers.GlorotNormal()
+
+    result = tf.keras.Sequential()
+
+    result.add(
+        tf.keras.layers.Conv2DTranspose(filters, size, strides=2, padding='same',
+                                        kernel_initializer=initializer, use_bias=False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if dropout:
+        result.add(tf.keras.layers.Dropout(0.25))
+
+    result.add(tf.keras.layers.ReLU())
+    return result
+
+
+def output_layer(size):
+    initializer = tf.keras.initializers.GlorotNormal()
+    return tf.keras.layers.Conv2DTranspose(CLASSES, size, strides=2, padding='same',
+                                           kernel_initializer=initializer, activation='sigmoid')
+
+
+inp_layer = input_layer()
+
+downsample_stack = [
+    downsample_block(64, 4, batch_norm=False),
+    downsample_block(128, 4),
+    downsample_block(256, 4),
+    downsample_block(512, 4),
+    downsample_block(512, 4),
+    downsample_block(512, 4),
+    downsample_block(512, 4),
+]
+
+upsample_stack = [
+    upsample_block(512, 4, dropout=True),
+    upsample_block(512, 4, dropout=True),
+    upsample_block(512, 4, dropout=True),
+    upsample_block(256, 4),
+    upsample_block(128, 4),
+    upsample_block(64, 4)
+]
+
+out_layer = output_layer(4)
+
+# Реализуем skip connections
+x = inp_layer
+
+downsample_skips = []
+
+for block in downsample_stack:
+    x = block(x)
+    downsample_skips.append(x)
+
+downsample_skips = reversed(downsample_skips[:-1])
+
+for up_block, down_block in zip(upsample_stack, downsample_skips):
+    x = up_block(x)
+    x = tf.keras.layers.Concatenate()([x, down_block])
+
+out_layer = out_layer(x)
+
+
+unet_like = tf.keras.Model(inputs=inp_layer, outputs=out_layer)
+unet_like.load_weights('./model/')
+
+#f = [filenames for _, _, filenames in walk("./data/test/")]
+frames = sorted(glob.glob('./data/test/*.jpg'))
+fig, ax = plt.subplots(nrows=2, ncols=5, figsize=(15, 5), dpi=125)
+
+for i in range(len(frames)):
+    frame = imread(frames[i])
+    sample = resize(frame, SAMPLE_SIZE)
+
+    predict = unet_like.predict(sample.reshape((1,) + SAMPLE_SIZE + (3,)))
+    predict = predict.reshape(SAMPLE_SIZE + (CLASSES,))
+
+    ax[0, i].set_title('Image')
+    ax[0, i].set_axis_off()
+    ax[0, i].imshow(frame)
+
+    ax[1, i].set_title('Mask')
+    ax[1, i].set_axis_off()
+    ax[1, i].imshow(predict / 1.5, interpolation='nearest')
+plt.show()
+plt.show()
 
 
 
-model = tf.keras.models.load_model('./model/model.h5')
-model.compile(optimizer=keras.optimizers.Adam(1e-3), loss="binary_crossentropy", metrics=["accuracy"],)
 
-test_image = tf.keras.preprocessing.image.load_img("./data/validation_data/class_people/1.jpg", target_size=image_size)
-test_image = tf.keras.preprocessing.image.img_to_array(test_image)
-test_image = numpy.expand_dims(test_image, axis=0)
-#test_image = test_image.reshape(image_size[0], image_size[1])
-result = model.predict(test_image)[0]
-print(f"This image is {100 * (1 - result)}% people and {100 * result}% no people.")
 
-test_image = tf.keras.preprocessing.image.load_img("./data/validation_data/class_empty/1.jpg", target_size=image_size)
-test_image = tf.keras.preprocessing.image.img_to_array(test_image)
-test_image = numpy.expand_dims(test_image, axis=0)
-#test_image = test_image.reshape(image_size[0], image_size[1])
-result = model.predict(test_image)[0]
-print(f"This image is {100 * (1 - result)}% people and {100 * result}% no people.")
+
