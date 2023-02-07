8ef18b7919b6880828a2ed6fc4d60c2a59f5c75f
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2023-02-07 18:02:01 +0300

    fix

--- data/image_tools/masks_creator.py
+++ data/image_tools/masks_creator.py
@@ -8,13 +8,13 @@ for i in range(n):
     tree = ET.parse(f'data/xmls/{i + 1}.xml')
     root = tree.getroot()
 
-    maska = Image.new('RGB', (256, 256), 'white')
+    maska = Image.new('RGB', (256, 256), 'black')
 
     idraw = ImageDraw.Draw(maska)
 
     data = [[j.find('bndbox')[t].text for t in range(len(j.find('bndbox')))] for j in root.findall('object')]
 
     for t in range(len(data)):
-        idraw.rectangle((int(data[t][0]), int(data[t][1]), int(data[t][2]), int(data[t][3])), fill='black')
+        idraw.rectangle((int(data[t][0]), int(data[t][1]), int(data[t][2]), int(data[t][3])), fill='white')
 
     maska.save(f'data/masks/{i + 1}.jpg')
