76843393139f7a044f34134e34bc64a888dda5ae
EmilGoryachih <83202732+EmilGoryachih@users.noreply.github.com>
2023-02-11 17:40:39 +0300

    Count_people

--- Show.py
+++ Show.py
@@ -1,18 +1,11 @@
-from skimage.io import imread
 from skimage.transform import resize
-import glob
 import numpy as np
-from matplotlib import pyplot as plt
 from functions import *
 import cv2
-from matplotlib import pyplot as plt
-from PIL import Image
 
 
 def return_mask(image):
-    #frame = imread(frames[i])
     sample = resize(image, SAMPLE_SIZE)
-
     predict = unet_like.predict(sample.reshape((1,) + SAMPLE_SIZE + (3,)))
     predict = predict.reshape(SAMPLE_SIZE)
     predict = np.where(predict < 0.1, 1, 0)
@@ -23,18 +16,14 @@ inp_layer, out_layer = create_layers()
 unet_like = tf.keras.Model(inputs=inp_layer, outputs=out_layer)
 unet_like.load_weights('./model/')
 
-# дальше код для визуализации, все для использования в реал тайм выше
-# кадры не нужно ресайзить, подаешь фото в виде масcива numpy
-
 cam = cv2.VideoCapture(0)
 hsv_min = np.array((2, 28, 65), np.uint8)
 hsv_max = np.array((26, 238, 255), np.uint8)
 print('ok')
 
-
-while (True):
+while True:
     _, frame = cam.read()
-    frame = cv2.resize(frame, (256, 256))
+    frame = cv2.resize(frame, (1920, 1080))
     nparray = np.array(frame)
     mask = return_mask(nparray)
     img = []
@@ -47,12 +36,13 @@ while (True):
                 gg.append([255, 255, 255])
         img.append(gg)
     img_to_use = np.array(img).astype(np.uint8)
-
+    img_to_use = cv2.resize(img_to_use, (1920, 1080))
     img_gray = cv2.cvtColor(img_to_use, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY)
-
     contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-
+    count_people = str(len(hierarchy[0]))
+    cv2.putText(img=frame, text=count_people, org=(50, 100), fontScale=5, fontFace=cv2.FONT_HERSHEY_PLAIN,
+                color=(255, 0, 0), thickness=5)
     cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0),
                      thickness=2, lineType=cv2.LINE_AA)
     #  contours, hierarchy = cv2.findContours(img_to_use, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -60,7 +50,6 @@ while (True):
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 
 cam.release()
 cv2.destroyAllWindows()
