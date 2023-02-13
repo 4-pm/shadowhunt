fec4c38da5796cf563232401c6c7c8f2a5774336
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2023-02-13 19:07:58 +0300

    add_empty_rooms
    fix code error

--- masks_creator.py
+++ masks_creator.py
@@ -7,16 +7,20 @@ my_path = os.getcwd()
 
 for i in os.listdir(f'{my_path}/data/peoples'):
     name = i[:-4]
-    tree = ET.parse(f'{my_path}/data/xmls/{name}.xml')
-    root = tree.getroot()
 
     maska = Image.new('RGB', (256, 256), 'black')
 
     idraw = ImageDraw.Draw(maska)
 
-    data = [[j.find('bndbox')[t].text for t in range(len(j.find('bndbox')))] for j in root.findall('object')]
+    try:
+        tree = ET.parse(f'{my_path}/data/xmls/{name}.xml')
+        root = tree.getroot()
 
-    for t in range(len(data)):
-        idraw.rectangle((int(data[t][0]), int(data[t][1]), int(data[t][2]), int(data[t][3])), fill='white')
+        data = [[j.find('bndbox')[t].text for t in range(len(j.find('bndbox')))] for j in root.findall('object')]
+
+        for t in range(len(data)):
+            idraw.rectangle((int(data[t][0]), int(data[t][1]), int(data[t][2]), int(data[t][3])), fill='white')
+    except FileNotFoundError:
+        pass
 
     maska.save(f'{my_path}/data/masks/{name}.jpg')
