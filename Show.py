a8f084cf6b4696aa615b1f4a65071936fa58e766
verdenkaa <nikitapahomov97@gmail.com>
2023-02-19 12:16:02 +0300

    fix artefacts

--- Show.py
+++ Show.py
@@ -16,8 +16,8 @@ inp_layer, out_layer = create_layers()
 unet_like = tf.keras.Model(inputs=inp_layer, outputs=out_layer)
 unet_like.load_weights('./model/')
 
-cam = cv2.VideoCapture(0)
-# cam = cv2.VideoCapture('20230210_125859.mp4')
+#cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture('3.mp4')
 hsv_min = np.array((2, 28, 65), np.uint8)
 hsv_max = np.array((26, 238, 255), np.uint8)
 print('ok')
@@ -32,15 +32,16 @@ while True:
     img_to_use = cv2.resize(img_to_use, (640, 450))
     img_gray = cv2.cvtColor(img_to_use, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-    try:
-        count_people = str(len(hierarchy[0]))
-    except TypeError:
-        count_people = '0'
-    cv2.putText(img=frame, text=count_people, org=(50, 100), fontScale=5, fontFace=cv2.FONT_HERSHEY_PLAIN,
-                color=(255, 0, 0), thickness=5)
-    cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0),
+    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+
+    count_people = 0
+    for i in contours:
+        if cv2.contourArea(i) > 1000:
+            count_people += 1
+            cv2.drawContours(image=frame, contours=i, contourIdx=-1, color=(0, 255, 0),
                      thickness=2, lineType=cv2.LINE_AA)
+    cv2.putText(img=frame, text=str(count_people), org=(50, 100), fontScale=5, fontFace=cv2.FONT_HERSHEY_PLAIN,
+                color=(255, 0, 0), thickness=5)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
